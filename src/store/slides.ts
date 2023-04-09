f087ea4cc08bd8ca74ba4af72350e51a211d1cd1
JfanLiu <1300083451@qq.com>
2023-04-09 14:12:52 +0800

    packet axios framework and build a navie convert bidirectionally

--- src/store/slides.ts
+++ src/store/slides.ts
@@ -5,6 +5,9 @@ import { Slide, SlideTheme, PPTElement, PPTAnimation, PPTTextElement } from '@/t
 import { slides } from '@/mocks/slides'
 import { theme } from '@/mocks/theme'
 import { layouts } from '@/mocks/layout'
+import { get_catalog, update_slides} from '@/api/ppt_Request_gpt'
+import { SetRequired } from 'type-fest'
+import { Chili } from '@icon-park/vue-next'
 
 interface RemoveElementPropData {
   id: string
@@ -187,7 +190,7 @@ export const useSlidesStore = defineStore('slides', {
       this.slides[slideIndex].elements = (elements as PPTElement[])
     },
 
-    convert_slide_to_dom(slide: Slide, root_name = 'section') {
+    convert_slide_to_dom(slide: Slide, root_name = 'slide') {
       // 功能：输入一页slide，提取有效信息为dom结构并返回
       // 说明：现只支持对text的提取。暂未保证顺序从上到下、从左到右 TODO
       // 返回值：dom结构，在顶层进行解析
@@ -199,33 +202,35 @@ export const useSlidesStore = defineStore('slides', {
 
       const elements = slide.elements // 引用传值
 
+      const parser = new DOMParser()
+
       for (let j = 0; j < elements.length; j++) {
         if (elements[j].type === 'text') {
           // 使用DOM 获取 XML 文档
           const textElement = elements[j] as PPTTextElement
-          const container = document.createElement('div')
-          container.innerHTML = textElement.content
-          // 获取元素并操作属性
 
-          console.log(container.firstChild)
-          const text = container.textContent
+          const top_Element_dom = parser.parseFromString(textElement.content, 'application/xml')
+          top_Element_dom.documentElement.setAttribute('id', textElement.id)
+          // top_Element_dom.documentElement.setAttribute('left', textElement.left.toString())
+          // top_Element_dom.documentElement.setAttribute('top', textElement.top.toString())
+          // top_Element_dom.documentElement.setAttribute('width', textElement.width.toString())
+          // top_Element_dom.documentElement.setAttribute('height', textElement.height.toString())
+          // top_Element_dom.documentElement.setAttribute('rotate', textElement.rotate.toString())
 
-          const p = document.createElement('p')
-          p.setAttribute('id', elements[j].id)
-          p.textContent = text
+          const text = top_Element_dom.documentElement.outerHTML
 
-          page.appendChild(p)
+          page.appendChild(top_Element_dom.documentElement)
         }
       }
       // 将xml转换为字符串
-      console.log(page.outerHTML)
+      // console.log(page.outerHTML)
 
       return page
     },
 
     convert_slides_to_dom(slides: Slide[], root_name = 'slides') {
       // 功能：将选中张幻灯片转换为dom
-      // 说明：调用convert_slide_to_dom
+      // 说明：调用convert_slide_to_dom。目前，外部只调用convert_slides_to_dom
       // 返回值：dom结构，在顶层进行解析
       
       const top_dom = document.createElement(root_name)
@@ -235,5 +240,143 @@ export const useSlidesStore = defineStore('slides', {
       }
       return top_dom
     },
+    traverse(node: ChildNode, depth = 0) {
+      // 打印当前节点的节点名称和深度
+      console.log(' '.repeat(depth * 2) + node.nodeName, node.ownerDocument)
+    
+      // 遍历子节点
+      for (const child of node.childNodes) {
+        this.traverse(child, depth + 1)
+      }
+    },
+    update_xml_to_dom_to_slide(xml: string, slides_all = slides) {
+      // 功能：将xml转换为dom，再更新到id对应的slide中
+      // 说明：根据xml中的slide_id去找到要更新的slide(因此传入所有幻灯片指针)，根据xml中的element_id去找到要更新的element
+      // 返回值：
+
+      const parser = new DOMParser()
+      const top_dom = parser.parseFromString(xml, 'application/xml')
+
+      if (top_dom.documentElement.nodeName === 'parsererror') {
+        console.error('XML 解析失败 when update_xml_to_dom_to_slide')
+      }
+      else {
+        console.log('XML 解析成功')
+
+        console.log(xml)
+
+        // 从根节点开始遍历
+        // this.traverse(top_dom.documentElement)
+
+        top_dom.querySelectorAll('slide').forEach((slide) => {
+          console.log(slide)
+          const slide_id = slide.getAttribute('id')
+          const slide_index = slides_all.findIndex((slide) => slide.id === slide_id)
+          if (slide_index === -1) {
+            console.error('未找到slide_id对应的slide')
+          }
+          else {
+            const inner_slide = slides_all[slide_index]
+            // 注意：这是遍历直接子级元素
+            let child: ChildNode | null = slide.firstChild
+            while (child) {
+
+              if (child.nodeType === Node.ELEMENT_NODE) {
+
+                console.log(child.nodeType, child.textContent, child instanceof Element)
+                const p = child as Element
+                const element_id = p.getAttribute('id')
+                const element_index = inner_slide.elements.findIndex((element) => element.id === element_id)
+                if (element_index === -1) {
+                  console.error(`未找到element_id${element_id}对应的element`)
+                }
+                else {
+                  const inner_textElement = inner_slide.elements[element_index] as PPTTextElement
+                  const target_textContent = p.textContent as string
+
+                  console.log('origin xml:', inner_textElement.content)
+                  console.log('target xml:', p.outerHTML)
+                  console.log('target txt:', target_textContent)
+
+                  const child_dom = parser.parseFromString(inner_textElement.content, 'application/xml')
+                  let node = child_dom.documentElement.childNodes[0]
+                  while (node.childNodes.length > 0) {
+                    node = node.childNodes[0]
+                  }
+
+                  console.log('要修改的结点值', node.textContent)
+                  node.textContent = target_textContent
+                  console.log('修改后的结点值', node.textContent)
+                  console.log('最终结果', child_dom.documentElement.outerHTML)
+                  
+                  inner_textElement.content = child_dom.documentElement.outerHTML
+                }
+              }
+
+              if (child?.nextSibling) {
+                child = child.nextSibling 
+              }
+              else {
+                break
+              }
+            }
+          }
+
+        })
+      }
+
+
+    },
+
+    request_update_slides(prompt: string): void {
+      const formData = new FormData()
+      formData.append('user_name', 'ljf')
+      formData.append('prompt', prompt)
+
+      const dom_top = this.convert_slides_to_dom(this.slides)
+      formData.append('ppt_xml', dom_top.outerHTML)
+
+      const receive_xml = `
+<slides>
+<slide id="test-slide-1">
+    <p id="idn7Mx"><strong><span style="font-size:  112px">论语</span></strong></p>
+    <p id="7stmVP"><span style="font-size:  24px">孔子的智慧与教导</span></p>
+</slide>
+<slide id="yH_-FXmhGU">
+    <p style="text-align: center;" id="tEyqPBBaml"><strong><span style="font-size: 112px">论语导言</span></strong></p>
+    <p style="" id="VCguVf4l1B"><span style="font-size: 24px">论语是儒家学派的经典文献，记录了孔子及其弟子的言行。</span></p>
+</slide>
+<slide id="Gh9ZjY8j3H">
+    <p style="text-align: center;" id="sL7xvxns-u"><strong><span style="font-size: 48px">目录</span></strong></p>
+    <ol style="list-style-type: lower-roman;" id="tS11ELDqgH">
+        <li><p style="text-align: center;"><span style="font-size: 28px">论语简介</span></p></li>
+        <li><p style="text-align: center;"><span style="font-size: 28px">孔子的思想</span></p></li>
+        <li><p style="text-align: center;"><span style="font-size: 28px">论语的影响</span></p></li>
+    </ol>
+</slide>
+<slide id="S_cO7EuNcV">
+    <p style="text-align: center;" id="07pzVgNyIZ"><strong><span style="font-size: 48px">论语简介</span></strong></p>
+    <ol style="list-style-type: lower-roman;" id="eIiAj3VJSZ">
+        <li><p style="text-align: center;"><span style="font-size: 28px">成书时间</span></p></li>
+        <li><p style="text-align: center;"><span style="font-size: 28px">作者及其弟子</span></p></li>
+        <li><p style="text-align: center;"><span style="font-size: 28px">内容结构</span></p></li>
+        <li><p style="text-align: center;"><span style="font-size: 28px">主要观点</span></p></li>
+    </ol>
+</slide>
+</slides>
+      `
+
+      this.update_xml_to_dom_to_slide(receive_xml, this.slides)
+
+      // update_slides(formData).then((response) => {
+      //   const a = response.data
+      //   console.log('res:', a)
+      //   console.log('res_json:', JSON.stringify(a, null, 2))
+      // }).catch(error => {
+      //   console.error('An error occurred:', error)
+      // })
+
+    },
+
   },
 })
--- src/views/Editor/GptChat/index.vue
+++ src/views/Editor/GptChat/index.vue
@@ -38,6 +38,7 @@ const { currentSlide } = storeToRefs(slidesStore)
 
 const gptinput = computed(() => currentSlide.value?.gptinput || '')
 
+slidesStore.request_update_slides('请帮我把这个ppt修改为论语主题')
 const commitInput = () => {
   // 功能：向后端提交输入的命令
   // 说明：点击按钮出发，输入不为空才提交，提交后清空输入框
